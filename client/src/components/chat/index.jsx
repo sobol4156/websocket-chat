689ea06974ecda202473d737cb89daeab5ae3b8f
Alex <147102432+sobol4156@users.noreply.github.com>
2024-05-03 02:07:56 +0300

    little bugfix

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
+import tg from '../../assets/img/telegram.png';
+import './chat.css'
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
@@ -10,7 +12,10 @@ const Chat = () => {
 
   useEffect(() => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: "smooth", block:'end' });
+      messagesEndRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
     }
   }, [messages]);
 
@@ -38,24 +43,34 @@ const Chat = () => {
     };
   }
 
+  // функция отправки сообщения на сокет
   const sendMessage = async () => {
     const message = {
       username,
       message: value,
       id: Date.now(),
       event: "message",
-      time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
     };
     socket.current.send(JSON.stringify(message));
     setValue("");
   };
-
+  //Остановка превычного поведения textarea для отправки сообщения через Enter
   const handleKeyDown = (event) => {
-    if (event.key === "Enter" && !event.ctrlKey) {
+    if (event.key === "Enter" && !event.ctrlKey && value) {
       event.preventDefault(); // Отменяем действие по умолчанию при нажатии Enter
       sendMessage();
     } else if (event.key === "Enter" && event.ctrlKey) {
       setValue((prev) => prev + "\n"); // Добавляем перенос строки в поле ввода при нажатии Ctrl+Enter
+    }
+  };
+  //UX подключение через Enter
+  const connectedChat = (event) => {
+    if (event.key === "Enter") {
+      connect();
     }
   };
 
@@ -69,7 +84,9 @@ const Chat = () => {
             onChange={(e) => setUsername(e.target.value)}
             type="text"
             placeholder="Введите ваше имя"
+            onKeyDown={connectedChat}
           />
+          
           <button className="bg-slate-700 rounded-3xl p-2" onClick={connect}>
             Вход
           </button>
@@ -77,12 +94,11 @@ const Chat = () => {
       </div>
     );
   }
-  const time = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
 
   return (
-    <div className="w-72 h-100 border rounded-2xl bg-[#282828] text-base flex flex-col">
+    <div className="w-72 h-100 border rounded bg-[#03161f] text-base flex flex-col">
       <div className="h-80 rounded-tl-2xl rounded-tr-2xl px-4 pb-3 overflow-y-auto flex flex-col-reverse">
-      <div ref={messagesEndRef}></div>
+        <div ref={messagesEndRef}></div>
         {messages.map((mess) => (
           <div className="" key={mess.id}>
             {mess.event === "connection" ? (
@@ -102,33 +118,31 @@ const Chat = () => {
                   style={{
                     maxWidth: "calc(100% - 60px)",
                     overflowWrap: "break-word",
+                    background: "#1376a8",
                   }}
                 >
+                  
                   {mess.message} <small>{mess.time}</small>
                 </span>
               </div>
             )}
           </div>
         ))}
-        
       </div>
       <div className="flex relative items-center">
         <textarea
-          className="rounded-tl-none rounded-bl-xl py-4 h-14 w-full text-[#282828] pl-2 pr-16 outline-none resize-none pt-4"
+          className="rounded-tl-none rounded py-4 h-14 w-full bg-[#17212b] pl-2 pr-16 outline-none resize-none pt-4"
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
           autoComplete="off"
           placeholder="Введите сообщение"
           onKeyDown={handleKeyDown}
+          
         ></textarea>
         <div>
-          <button
-            onClick={sendMessage}
-            className="right-2 bg-slate-700  rounded-br-2xl w-full h-14 hover:bg-gray-800"
-          >
-            Send
-          </button>
+          {value? <img src={tg} alt="Message" className="rotate-45 absolute right-5 bottom-3 cursor-pointer" onClick={sendMessage}/> : ""}
+        
         </div>
       </div>
     </div>
